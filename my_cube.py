5bcc636427efe0adc07ba4ae0288ba4d9d50ff71
John Bradford <jwgbradford@gmail.com>
2024-03-02 16:00:44 +0000

    added rotation to cube class

--- my_cube.py
+++ my_cube.py
@@ -3,3 +3,10 @@ from ursina import Entity
 class MyCube(Entity):
     def __init__(self, add_to_scene_entities=True, **kwargs):
         super().__init__(add_to_scene_entities, **kwargs)
+        for key, value in kwargs.items():
+            setattr(self, key, value)
+
+    def update(self) -> None:
+        self.rotation_x += 1
+        self.rotation_y += 1
+        self.rotation_z += 1
