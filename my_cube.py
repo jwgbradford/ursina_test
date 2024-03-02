886c72ab683dd09794c2bbfe20e88132c6920ea2
John Bradford <jwgbradford@gmail.com>
2024-03-02 16:24:50 +0000

    added default rotation keys

--- my_cube.py
+++ my_cube.py
@@ -4,14 +4,21 @@ from ursina import held_keys, time
 class MyCube(Entity):
     def __init__(self, add_to_scene_entities=True, **kwargs):
         super().__init__(add_to_scene_entities, **kwargs)
+        # default aswd controls
+        self.controls = {
+            "up" : "a",
+            "down" : "s",
+            "left" : "d",
+            "right" : "a"
+        }
         for key, value in kwargs.items():
             setattr(self, key, value)
 
     def update(self) -> None:
         self.rotation_x += (
-            (held_keys[self.controls['up']] - held_keys[self.controls['down']]) 
+            (held_keys[self.controls['down']] - held_keys[self.controls['up']]) 
             * time.dt * 100)
         #self.rotation_y += 1
         self.rotation_z += (
-            (held_keys[self.controls['left']] - held_keys[self.controls['right']]) 
+            (held_keys[self.controls['right']] - held_keys[self.controls['left']]) 
             * time.dt * 100)
