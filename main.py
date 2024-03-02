7868749b4fee8b6d35030580264a1f8bec474e01
John Bradford <jwgbradford@gmail.com>
2024-03-02 16:19:33 +0000

    added rotational control

--- main.py
+++ main.py
@@ -1,17 +1,28 @@
-from ursina import Ursina, Entity
+from ursina import Ursina
 from ursina import color
 from my_cube import MyCube
+from json import load
 
 class MyGame:
     def __init__(self) -> None:
         self.app = Ursina()
 
+    def load_kwargs(self, key) -> dict:
+        with open("controls.json") as input_file:
+            input_data = load(input_file)
+        print(input_data)
+        dict_data = input_data[key]
+        return dict_data
+
     def add_my_block(self) -> None:
+        controls = self.load_kwargs('movement')
+        print(controls)
         self.my_block = MyCube(
             model = 'cube', 
             color = color.orange, 
             texture = 'brick', 
-            rotation=(45,0,45)
+            rotation=(45,0,45),
+            controls = controls
         )
 
     def run(self) -> None:
--- my_cube.py
+++ my_cube.py
@@ -1,4 +1,5 @@
 from ursina import Entity
+from ursina import held_keys, time
 
 class MyCube(Entity):
     def __init__(self, add_to_scene_entities=True, **kwargs):
@@ -7,6 +8,10 @@ class MyCube(Entity):
             setattr(self, key, value)
 
     def update(self) -> None:
-        self.rotation_x += 1
-        self.rotation_y += 1
-        self.rotation_z += 1
+        self.rotation_x += (
+            (held_keys[self.controls['up']] - held_keys[self.controls['down']]) 
+            * time.dt * 100)
+        #self.rotation_y += 1
+        self.rotation_z += (
+            (held_keys[self.controls['left']] - held_keys[self.controls['right']]) 
+            * time.dt * 100)
