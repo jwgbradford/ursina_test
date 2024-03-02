33caeda67cc1a244e4afbc332a841e9ae321eb38
John Bradford <jwgbradford@gmail.com>
2024-03-02 15:53:29 +0000

    tidy code up

--- main.py
+++ main.py
@@ -5,17 +5,16 @@ class MyGame:
     def __init__(self) -> None:
         self.app = Ursina()
 
-    def add_entities(self) -> None:
-        self.my_ball = Entity(
+    def add_my_block(self) -> None:
+        self.my_block = Entity(
             model = 'cube', 
             color = color.orange, 
             texture = 'brick', 
             rotation=(45,0,45)
         )
 
-
     def run(self) -> None:
-        self.add_entities()
+        self.add_my_block()
         self.app.run()
 
 if __name__ == '__main__':
