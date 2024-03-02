e3a7f155c5e8360618834b73017c209ecb268c77
John Bradford <jwgbradford@gmail.com>
2024-03-02 15:48:09 +0000

    add a simple ball

--- main.py
+++ main.py
@@ -1,10 +1,16 @@
-from ursina import Ursina
+from ursina import Ursina, Entity
+from ursina import color
 
 class MyGame:
     def __init__(self) -> None:
         self.app = Ursina()
 
+    def add_entities(self) -> None:
+        self.my_ball = Entity(model = 'sphere')
+
+
     def run(self) -> None:
+        self.add_entities()
         self.app.run()
 
 if __name__ == '__main__':
